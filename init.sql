724d9fbbb1a0e64f54e204429235eb47c18a066c
Nguyen Tran <ntran272@csu.fullerton.edu>
2018-09-17 02:11:37 -0700

    Add files via upload

--- init.sql
+++ init.sql
@@ -1,1 +1,29 @@
-CREATE TABLE user (id INTEGER PRIMARY KEY ASC, username TEXT, password TEXT);
+CREATE TABLE  IF NOT EXISTS user(
+	id INTEGER PRIMARY KEY ASC, 
+	username TEXT, 
+	password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS forums(
+	id INTEGER PRIMARY KEY ASC, 
+	forum_name TEXT, 
+	forum_creator INTEGER,
+	forum_time DATETIME,
+	FOREIGN KEY (forum_creator) REFERENCES user(id)
+);
+
+CREATE TABLE IF NOT EXISTS threads(
+	thread_id INTEGER PRIMARY KEY ASC,
+	thread_title TEXT NOT NULL,
+	thread_creator INTEGER NOT NULL,
+	thread_time TEXT,
+	forum_id INTEGER,
+	FOREIGN KEY (forum_id) REFERENCES forums(id),
+	FOREIGN KEY (thread_creator) REFERENCES user(id)
+);
+
+
+
+INSERT INTO USER VALUES (1, "hoangphuc", "love" );
+INSERT INTO FORUMS VALUES (1, "Testing", 1, "2017-09-16 15:33:33.12343" );
+INSERT INTO THREADS VALUES (1,"Whydo?", 1, "2017-09-14 15:33:33.123433",1);
